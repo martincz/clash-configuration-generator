c0d3a88b2a47a4c9fc2d29c6d15dc96f92ee019b
Martincz Gao <martincz@mokeedev.com>
2022-03-30 17:26:52 +0800

    优化逻辑、拆分自定义规则文件

--- generate.py
+++ generate.py
@@ -34,29 +34,30 @@ def main():
     with open('configs/proxy.yaml') as fp:
         cfg_proxy = yaml.load(fp)
 
+    with open('configs/dns.yaml') as fp:
+        cfg_dns = yaml.load(fp)
+
+    with open('configs/custom.yaml') as fp:
+        cfg_custom = yaml.load(fp)
+
+    # 合并所有规则
     with open('rules/common.yaml') as fp:
         rules_common = yaml.load(fp)
 
     with open('rules/custom.yaml') as fp:
         rules_custom = yaml.load(fp)
-        rules_final = rules_common.copy()
 
-        rules_final['rules'] = rules_common['rules'] + rules_custom['rules']
-        # rules_final['rules'].append(rules_custom['rules'])
+    with open('rules/suffix.yaml') as fp:
+        rules_suffix = yaml.load(fp)
 
-    with open('configs/dns.yaml') as fp:
-        cfg_dns = yaml.load(fp)
+    rules_final = rules_common.copy()
+    rules_final['rules'] = rules_common['rules'] + rules_custom['rules'] + rules_suffix['rules']
 
+    # 整合最终配置
     cfg_final = cfg_common.copy()
-    cfg_arrays = [cfg_proxy, rules_final, cfg_dns]
-
+    cfg_arrays = [cfg_proxy, rules_final, cfg_dns, cfg_custom]
     for cfg in cfg_arrays:
         cfg_final.update(cfg)
-
-    with open('configs/custom.yaml') as fp:
-        cfg_custom = yaml.load(fp)
-        cfg_final.update(cfg_custom)
-
     with open('configuration.yaml', 'w') as fp:
         yaml.dump(cfg_final, fp)
 
